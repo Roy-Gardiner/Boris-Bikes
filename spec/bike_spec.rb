0709f6db4b658e4ca6c2b5ad39a43077c6d919aa
Roy Gardiner <roy.gardiner@btopenworld.com>
2014-01-20 14:10:46 +0000

    first classes defined

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,6 +4,6 @@ require_relative "../lib/bike"
 describe Bike do 
   it "should not be broken after we create it"	do 
   	bike = Bike.new
-  	expect (bike).not_to be_broken
+  	expect(bike).not_to be_broken
   end
 end
